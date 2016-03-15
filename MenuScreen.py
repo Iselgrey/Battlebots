58c62062bc92e10419af8b8014b708bf0c6f3433
Iselgrey <jpdehesagolding@gmail.com>
2016-03-15 16:54:10 +0100

    Fix dictionary listing keys

--- MenuScreen.py
+++ MenuScreen.py
@@ -33,11 +33,11 @@ class MenuScreen(AbstractScreen):
         label_start_rect = self.label_start.get_rect()
         label_start_rect.center = rect.centerx, rect.height*3/4
         surface.blit(self.label_start, label_start_rect)
-        label_p = self.font.render(("> " if self.bot_select == 0 else "  ") + "PLAYER 1: " + self.bot_list.keys()[self.bot_p1], 1, self.COLOR_WHITE)
+        label_p = self.font.render(("> " if self.bot_select == 0 else "  ") + "PLAYER 1: " + list(self.bot_list.keys())[self.bot_p1], 1, self.COLOR_WHITE)
         label_p_rect = label_p.get_rect()
         label_p_rect.center = rect.centerx, rect.height*18/40
         surface.blit(label_p, label_p_rect)
-        label_p = self.font.render(("> " if self.bot_select == 1 else "  ") + "PLAYER 2: " + self.bot_list.keys()[self.bot_p2], 1, self.COLOR_WHITE)
+        label_p = self.font.render(("> " if self.bot_select == 1 else "  ") + "PLAYER 2: " + list(self.bot_list.keys())[self.bot_p2], 1, self.COLOR_WHITE)
         label_p_rect = label_p.get_rect()
         label_p_rect.center = rect.centerx, rect.height*22/40
         surface.blit(label_p, label_p_rect)
@@ -49,8 +49,8 @@ class MenuScreen(AbstractScreen):
             if event.key == K_ESCAPE:
                 self.action(self.ACTION_QUIT, None)
             elif event.key == K_SPACE:
-                self.action(self.ACTION_SETP1, self.bot_list[self.bot_list.keys()[self.bot_p1]])
-                self.action(self.ACTION_SETP2, self.bot_list[self.bot_list.keys()[self.bot_p2]])
+                self.action(self.ACTION_SETP1, self.bot_list[list(self.bot_list.keys())[self.bot_p1]])
+                self.action(self.ACTION_SETP2, self.bot_list[list(self.bot_list.keys())[self.bot_p2]])
                 self.action(self.ACTION_STARTGAME, None)
             elif event.key == K_UP:
                 self.bot_select = self.bot_select - 1
